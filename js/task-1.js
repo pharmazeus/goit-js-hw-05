681888e1c27f33de5c30596fbfda60a62b530465
Vladyslav Maidanskyi <markmaydeens@gmail.com>
2024-12-06 22:30:50 -0500

    Initial commit

---
+++ js/task-1.js
@@ -0,0 +1,19 @@
+const isEnoughCapacity = function (products, containerSize) {
+  const quantityOfProducts = Object.values(products);
+  let totalProds = 0;
+  for (const unit of quantityOfProducts) {
+    totalProds += unit;
+  }
+  if (totalProds <= containerSize) {
+    return true;
+  }
+  return false;
+};
+
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
+
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
